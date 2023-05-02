2e64e87e90559551401e4f56a008c7a422394e11
ZoDSeR SyLVaN <zodser97@gmail.com>
2023-05-02 09:19:30 -0400

    updated to MVC

---
+++ DojoSurvey/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurvey.Models;
+
+namespace DojoSurvey.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()=>View();
+
+    [HttpGet("results")]
+    public IActionResult Result(Survey data) => View(data);
+
+    [HttpPost("process")]
+    public IActionResult Process(Survey data)=> RedirectToAction("Result", data);
+    
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ DojoSurvey/Models/SurveyModel.cs
@@ -0,0 +1,7 @@
+namespace DojoSurvey.Models;
+public class Survey{
+    public string name {get; set;}
+    public string location {get; set;}
+    public string favLang {get; set;}
+    public string comment {get; set;}
+}
