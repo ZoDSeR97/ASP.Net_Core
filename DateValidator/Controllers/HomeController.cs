60b669e480e8b9412c167c799e20e8a227984eb4
ZoDSeR SyLVaN <zodser97@gmail.com>
2023-05-02 14:57:49 -0400

    DateTime custom validation

---
+++ DateValidator/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DateValidator.Models;
+
+namespace DateValidator.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()=>View("Index");
+
+    [HttpPost("process")]
+    public IActionResult Process(Date Date)=>ModelState.IsValid?RedirectToAction("Index"):Index();
+
+    public IActionResult Privacy()=>View();
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ DateValidator/Models/DateModel.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+namespace DateValidator.Models;
+
+public class Date{
+    [Required]
+    [FutureDate]
+    public DateTime? D {get; set;}
+}
+
+public class FutureDateAttribute : ValidationAttribute
+{    
+    protected override ValidationResult IsValid(object? value, ValidationContext validationContext)    
+    {        
+        // You first may want to unbox "value" here and cast to to a DateTime variable!
+        if(DateTime.Compare((DateTime)value, DateTime.Today)<0)
+            return ValidationResult.Success;
+        return new ValidationResult("Date is not in the past");
+    }
+}
